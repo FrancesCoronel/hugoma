a44d0ba93970df069af8c7ae47dd5d9fe422d208
Frances Coronel <hello@fvcproductions.com>
2018-07-23 21:21:34 -0700

    Change sw.js config
    Signed-off-by: Frances Coronel <hello@fvcproductions.com>

--- webpack.config.js
+++ webpack.config.js
@@ -10,36 +10,35 @@ const CompressionPlugin = require("compression-webpack-plugin");
 
 export default {
   module: {
-    rules: [
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader"
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-c9])?$/,
-        loader: "url-loader"
-      },
-      {
-        test: /\.js?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-        query: {
+    rules: [{
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: "url-loader"
+    },
+    {
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-c9])?$/,
+      loader: "url-loader"
+    },
+    {
+      test: /\.js?$/,
+      loader: "babel-loader",
+      exclude: /node_modules/,
+      query: {
           cacheDirectory: true
         }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
+    },
+    {
+      test: /\.css$/,
+      use: ["style-loader", "css-loader"]
+    },
+    {
+      test: /\.(gif|png|jpe?g|svg)$/i,
+      use: [
           "file-loader",
           {
             loader: "image-webpack-loader"
           }
         ]
-      }
+    }
     ]
   },
   plugins: [
@@ -75,22 +74,22 @@ export default {
       swDest: path.join("dist", "sw.js"),
       clientsClaim: true,
       skipWaiting: true,
-      runtimeCaching: [
-        {
-          urlPattern: /\.(?:png|gif|jpg)$/,
-          handler: "networkFirst",
-          options: {
-            cacheName: "image-cache"
-          }
-        },
-        {
-          urlPattern: new RegExp("https://google-analytics.com"),
-          handler: "staleWhileRevalidate"
-        },
-        {
-          urlPattern: new RegExp("https://twemoji.maxcdn.com"),
-          handler: "staleWhileRevalidate"
-        }
+      runtimeCaching: [{
+        urlPattern: /\.(?:html)$/,
+        handler: "networkFirst",
+      },
+      {
+        urlPattern: /\.(?:css|png|gif|jpg|svg|xml|js|ico|json)$/,
+        handler: "staleWhileRevalidate",
+      },
+      {
+        urlPattern: new RegExp("https://google-analytics.com"),
+        handler: "staleWhileRevalidate"
+      },
+      {
+        urlPattern: new RegExp("https://twemoji.maxcdn.com"),
+        handler: "staleWhileRevalidate"
+      }
       ]
     })
   ],
